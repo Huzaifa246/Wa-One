28ce7c51d6dd14961f9a73aba2db12906d7e0385
Huzaifa246 <huzaifa24680@gmail.com>
2023-04-26 21:55:25 -0700

    first commit

--- src/constant/table-data.js
+++ src/constant/table-data.js
@@ -1,5 +1,5 @@
 import user1 from "@/assets/images/users/user-1.jpg";
-import user2 from "@/assets/images/users/user-567.jpg";
+import user2 from "@/assets/images/users/user-2.jpg";
 import user3 from "@/assets/images/users/user-3.jpg";
 import user4 from "@/assets/images/users/user-4.jpg";
 import user5 from "@/assets/images/users/user-5.jpg";
